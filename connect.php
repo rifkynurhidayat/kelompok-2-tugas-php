555cc9d8d9ff83f45248d98ec5cae8b3eae3648b
Kuninganunited20 <rifkynurhidayat623@gmail.com>
2023-05-10 22:06:07 +0700

    koneksi

---
+++ connect.php
@@ -0,0 +1,7 @@
+<?php
+    $hostName   = "localhost";
+    $userName   = "root";
+    $password   = "";
+    $dbName     = "perpus";
+    $conn       = new mysqli($hostName, $userName, $password,$dbName);
+?>
