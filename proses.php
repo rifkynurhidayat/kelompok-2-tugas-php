df48b739196aa704ad6ca92a54fddf912165f417
Kuninganunited20 <rifkynurhidayat623@gmail.com>
2023-05-21 20:54:00 +0700

    proses.php

--- proses.php
+++ proses.php
@@ -1,12 +1,11 @@
 <?php
+include ('connect.php');
 $id = $_POST['id_pengarang'];
 $pengarang = $_POST['nama_pengarang'];
 $email = $_POST['email'];
 $telepon = $_POST['telp'];
 $alamat = $_POST['alamat'];
 
-include ('connect.php');
-
 $result = mysqli_query($conn, "INSERT INTO `pengarang` (`id_pengarang`,`nama_pengarang`,`email`,`telp`,`alamat`) VALUES ('$id','$pengarang','$email','$telepon','$alamat'); ");
-header("Location:index.php");
+header("Location: index.php");
 ?>
