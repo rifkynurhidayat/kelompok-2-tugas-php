2c355688ceb5794b1413223b45532c6291722c58
Kuninganunited20 <rifkynurhidayat623@gmail.com>
2023-05-21 20:53:16 +0700

    create.php

--- create.php
+++ create.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <title>create</title>
+    <script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 <body>
     <div class="container">
@@ -45,82 +45,55 @@
                 </tr>
             </table>
             <td>
-                <button id="my-button" type="button" name="submit" class="btn btn-primary mt-4 mb-4">Submit</button>
+                <button id="my-button" type="button" class="btn btn-primary mt-4 mb-4">Submit</button>
             </td>
         </form>
     </div>
 </body>
 </html>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 <script type="text/javascript">
-    $(document).ready(function() {
-        $('#my-button').click(function(event) {
+        $('#my-button').click(function() {
                 if ($('#id_pengarang').val().length == 0){
-                    $('#id_pengarang' === '') 
-                    $('#id').show().text('isi id dahulu');
-                     $('#id_pengarang').css({"backgroundColor" : "red"});
-                } else if('#id_pengarang' !== '' && '#id' !== ''){
+                  $('#id_pengarang' === '');
+                    $('#id').show().text('*isi id dahulu');
+                    $('#id').css({"font-style":"italic"});
+                     $('#id').css({"color" : "red"});
+                } else{ $('#id_pengarang' !== '' && '#id' !== '')
                     $('#id').hide();
-                    $('#id_pengarang').css('background-color', '');
-                }
-                if  ($('#nama_pengarang').val().length == 0){
-                    $('#nama_pengarang' === '')
-                    $('#nama').show().text('isi nama dahulu');
-                    $('#nama_pengarang').css({"backgroundColor" : "red"});
-                }else if ('#nama_pengarang' !== '' && '#nama' !== ''){
+                }if  ($('#nama_pengarang').val().length == 0){
+                    $('#nama_pengarang' === '');
+                    $('#nama').show().text('*isi nama dahulu');
+                    $('#nama').css({"font-style":"italic"});
+                    $('#nama').css({"color" : "red"});
+                }else { $('#nama_pengarang' !== '' && '#nama' !== '')
                     $('#nama').hide();
-                    $('#nama_pengarang').css('background-color', '');
-                }
-                if ($('#email').val().length == 0){
-                    $('#email' == '')
-                    $('#email-warning').show().text('isi email dahulu');
-                    $('#email').css({"backgroundColor" : "red"});
-                } else if('#email' !== '' && '#email-warning' !== ''){
+                }if ($('#email').val().length == 0){
+                    $('#email' == '');
+                    $('#email-warning').show().text('*isi email dahulu');
+                    $('#email-warning').css({"font-style":"italic"});
+                    $('#email-warning').css({"color" : "red"});
+                } else {$ ('#email' !== '' && '#email-warning' !== '')
                     $('#email-warning').hide();
-                    $('#email').css('background-color','');
-                } 
-                if ($('#telp').val().length == 0){
-                    $('#telp' == '')
-                   $('#telp-warning').show().text('isi nomer telepon dahulu');
-                   $('#telp').css({"backgroundColor" : "red"});
-                } else if('#telp' !== '' && '#telp-warning' !== ''){
+                } if ($('#telp').val().length == 0){
+                    $('#telp' == '');
+                   $('#telp-warning').show().text('*isi nomer telepon dahulu');
+                   $('#telp-warning').css({"font-style":"italic"});
+                   $('#telp-warning').css({"color" : "red"});
+                } else { $('#telp' !== '' && '#telp-warning' !== '')
                     $('#telp-warning').hide();
-                    $('#telp').css('background-color', '');
-                }
-                if ($('#alamat').val().length == 0){
-                    $('#alamat' == '')
-                    $('#alamat-warning').show().text('isi alamat dahulu');
-                    $('#alamat').css({"backgroundColor" : "red"});
-                }else if('#alamat' !== '' && '#alamat-warning' !== ''){
+                }if ($('#alamat').val().length == 0 ){
+                    $('#alamat' == '');
+                    $('#alamat-warning').show().text('*isi alamat dahulu');
+                    $('#alamat-warning').css({"font-style":"italic"});
+                    $('#alamat-warning').css({"color" : "red"});
+                }else { $('#alamat' !== '' && '#alamat-warning' !== '')
                     $('#alamat-warning').hide();
-                    $('#alamat').css('background-color','');
-                } else{
-                    $('#form-pengarang').submit();
-                } 
-                var id = $('#id_pengarang').val();
-                var nama = $('#nama_pengarang').val();;
-                var email = $('#email').val();;
-                var telepon = $('#telp').val();;
-                var alamat = $('#alamat').val();;
-                
-                var data = {
-                    id:id,
-                    nama:nama,
-                    email:email,
-                    telepon:telepon,
-                    alamat:alamat
-                };
-
-                $.ajax ({
-                    url: 'proses.php',
-                    type: 'POST',
-                    data: data,
-                    success: function(response) {
-                        console.log(response);
-                    },
-                    error:function(xhr, status, error) {
-                        console.log(error);
+                }
+                if ($('#id_pengarang').val().length > 0 && $('#nama_pengarang').val().length > 0 && $('#email').val().length > 0 && $('#telp').val().length > 0 && ($('#alamat').val().length > 0 || $('#alamat').val() != '')) {
+                 $('#form-pengarang').submit();
                     }
-                });
-        });
-    });
+            });
 </script>
+
+
